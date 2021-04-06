0624268d1cb8f65c5fa787c6396d9c75c00bc7d5
Glitch (hello-webpage) <none>
2021-04-06 14:19:46 +0000

    👓👶 Checkpoint ./up-flip-down.js:30458638/1866 ./index.html:30458638/99

--- up-flip-down.js
+++ up-flip-down.js
@@ -1,9 +1,25 @@
 AFRAME.registerComponent('up-flip-down', {
   init: function () {
     let el = this.el;
+    this.animateFlip = function() {
+      console.log(el.getAttribute());
+      let currPosition = el.getAttribute('position');
+      let params = {
+        property: 'position',
+        to: {
+          x: currPosition.x,
+          y: currPosition.y+3,
+          z: currPosition.z
+        },
+        dur: 1000,
+        easing: 'easeInOutQuad'
+      };
+      el.setAttribute('animation', params);
+    }
     
+    this.el.addEventListener('click', this.animateFlip);
   },
   remove: function() {
-    
+    this.el.removeEventListener('click', this.animateFlip);
   }
 })
