3608c393b3cd607faca7ce7e68cf4dacdc5ca20b
Glitch (hello-webpage) <none>
2021-04-07 20:46:25 +0000

    👯💴 Checkpoint ./util.js:30458638/313

--- util.js
+++ util.js
@@ -1,3 +1,14 @@
 function findNearestGridCenter(x, y) {
+  if (x <= -4 || x => 4 || y <= -4 || y >= 4) { // in case of edge of the board
+    return null;
+  }
   
+  if (round(x) === x || round(y) === y) { // right in the middle of two grid (extremely rare case)
+    return null;
+  }
+  
+  if (x < -3.5) {
+    x = 3.5
+  }
+   else if (x > 3.5)
 }
