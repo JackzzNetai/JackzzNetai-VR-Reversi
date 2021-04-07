95c71d1f77392a3006d4f06d7b9e6b8ae9460248
Glitch (hello-webpage) <none>
2021-04-07 14:23:15 +0000

    👐🐾 Checkpoint ./slide-up-down.js:30458638/10620

--- slide-up-down.js
+++ slide-up-down.js
@@ -17,9 +17,59 @@ AFRAME.registerComponent("slide-up-down", {
       el.setAttribute("animation", params);
     };
 
+    this.slideUp = function() {
+      let p = el.getAttribute("position");
+      let params = {
+        property: "position",
+        to: {
+          x: p.x,
+          y: p.y + 1,
+          z: p.z
+        },
+        dur: 500,
+        easing: "linear"
+      };
+      el.setAttribute("animation", params);
+    };
+
+    this.slideUpDown = function(e) {
+      if (e.detail.y > 0.95) {
+        let p = el.getAttribute("position");
+        let params = {
+          property: "position",
+          to: {
+            x: p.x,
+            y: p.y - 1,
+            z: p.z
+          },
+          dur: 500,
+          easing: "linear"
+        };
+        el.setAttribute("animation", params);
+      }
+      if (e.detail.y < -0.95) {
+        let p = el.getAttribute("position");
+        let params = {
+          property: "position",
+          to: {
+            x: p.x,
+            y: p.y + 1,
+            z: p.z
+          },
+          dur: 500,
+          easing: "linear"
+        };
+        el.setAttribute("animation", params);
+      }
+    };
+
     this.el.sceneEl.addEventListener("abuttondown", this.slideDown);
+    this.el.sceneEl.addEventListener("bbuttondown", this.slideUp);
+    this.el.sceneEl.addEventListener("thumbstickmoved", this.slideUpDown);
   },
   remove: function() {
     this.el.sceneEl.removeEventListener("abuttondown", this.slideDown);
+    this.el.sceneEl.removeEventListener("bbuttondown", this.slideUp);
+    this.el.sceneEl.removeEventListener("thumbstickmoved", this.slideUpDown);
   }
 });
