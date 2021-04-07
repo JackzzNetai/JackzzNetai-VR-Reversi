58b71fc67e16eebcbc10df305bce21f7cdfddea2
Glitch (hello-webpage) <none>
2021-04-07 20:01:06 +0000

    🛩🎹 Checkpoint ./place-piece.js:30458638/1406

--- place-piece.js
+++ place-piece.js
@@ -11,8 +11,27 @@ AFRAME.registerComponent('place-piece', {
       console.log(boardY + PIECE_DEFAULT_Y - BOARD_DEFAULT_Y);
       
       let newPiece = document.createElement('a-entity');
+      let whiteCylinder = document.createElement('a-cylinder');
+      whiteCylinder.setAttribute('radius', "0.4");
+      whiteCylinder.setAttribute('height', "0.1");
+      whiteCylinder.setAttribute('color', "#FFFFFF");
+      let blackCylinder = document.createElement('a-cylinder');
+      blackCylinder.setAttribute('radius', "0.4");
+      blackCylinder.setAttribute('height', "0.1");
+      blackCylinder.setAttribute('color', "#000000");
+      if (turn % 2 == 1) { //white's turn
+        whiteCylinder.setAttribute('position', "0 .05 0");
+        blackCylinder.setAttribute('position', "0 -.05 0");
+      } else { //black's turn
+        whiteCylinder.setAttribute('position', "0 -.05 0");
+        blackCylinder.setAttribute('position', "0 .05 0");
+      }
+      
+      newPiece.appendChild(whiteCylinder);
+      newPiece.appendChild(whiteCylinder);
       newPiece.setAttribute('', );
       
+      turn += 1;
     }
     
     this.el.addEventListener('click', this.placePiece);
@@ -23,4 +42,17 @@ AFRAME.registerComponent('place-piece', {
 });
 
 
-
+<a-entity position=".5 .5 -1.5" up-flip-down slide-up-down>
+        <a-cylinder
+          position="0 .05 0"
+          radius="0.4"
+          height="0.1"
+          color="#FFFFFF"
+        ></a-cylinder>
+        <a-cylinder
+          position="0 -.05 0"
+          radius="0.4"
+          height="0.1"
+          color="#000000"
+        ></a-cylinder>
+      </a-entity>
