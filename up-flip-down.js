26e05a83d5b9e674ca829cc10ee297e1ee50724b
Glitch (hello-webpage) <none>
2021-04-07 01:22:57 +0000

    🕴💗 Checkpoint ./up-flip-down.js:30458638/1770

--- up-flip-down.js
+++ up-flip-down.js
@@ -1,8 +1,8 @@
 AFRAME.registerComponent('up-flip-down', {
   init: function () {
     let el = this.el;
-    this.animateFlip = function(e) {
-      console.log(e.detail.intersection.point);
+    
+    this.animateFlip = function() {
       let currPosition = el.getAttribute('position');
       let params = {
         property: 'position',
@@ -15,17 +15,19 @@ AFRAME.registerComponent('up-flip-down', {
         easing: 'easeInOutQuad'
       };
       el.setAttribute('animation', params);
-      params = {
-        property: 'position',
-        to: {
-          x: currPosition.x,
-          y: currPosition.y+4,
-          z: currPosition.z
-        },
-        dur: 1000,
-        easing: 'easeInOutQuad'
-      };
-      el.setAttribute('animation', params);
+      setTimeout(function() {
+        let params = {
+          property: 'position',
+          to: {
+            x: currPosition.x,
+            y: currPosition.y-3,
+            z: currPosition.z
+          },
+          dur: 1000,
+          easing: 'easeInOutQuad'
+        };
+        el.setAttribute('animation', params);
+      }, 2000);
     }
     
     this.el.addEventListener('click', this.animateFlip);
