bb3e6fe0c082478fb4bbb7ddecf2563b74d5fe53
Glitch (hello-webpage) <none>
2021-04-08 08:57:50 +0000

    🤘🍶 Checkpoint ./place-piece.js:30458638/1198 ./Game.js:30458638/12 ./util.js:30458638/703

--- place-piece.js
+++ place-piece.js
@@ -18,31 +18,14 @@ AFRAME.registerComponent('place-piece', {
       
       //check validity
       let xzPair = findNearestGridCenter(x, z);
-      let 
-      
-      let newPiece = document.createElement('a-entity');
-      let whiteCylinder = document.createElement('a-cylinder');
-      whiteCylinder.setAttribute('radius', "0.4");
-      whiteCylinder.setAttribute('height', "0.1");
-      whiteCylinder.setAttribute('color', "#FFFFFF");
-      let blackCylinder = document.createElement('a-cylinder');
-      blackCylinder.setAttribute('radius', "0.4");
-      blackCylinder.setAttribute('height', "0.1");
-      blackCylinder.setAttribute('color', "#000000");
-      if (game.currPlayer == false) { //white's turn
-        whiteCylinder.setAttribute('position', "0 .05 0");
-        blackCylinder.setAttribute('position', "0 -.05 0");
-      } else { //black's turn
-        whiteCylinder.setAttribute('position', "0 -.05 0");
-        blackCylinder.setAttribute('position', "0 .05 0");
+      if (!game.isValidMove(game.convertXZCoordinateToPosIndex(xzPair), game.currPlayer)) {
+        return;
       }
-      newPiece.appendChild(whiteCylinder);
-      newPiece.appendChild(blackCylinder);
+      
+      let newPiece = makeWhitePiece('ore');
       
       let boardY = document.getElementById("board").getAttribute('position').y;
       newPiece.setAttribute('position', {x: xzPair[0], y: boardY + PIECE_DEFAULT_Y - BOARD_DEFAULT_Y, z: xzPair[1]});
-      newPiece.setAttribute('up-flip-down', {});
-      newPiece.setAttribute('slide-up-down', {});
       document.querySelector('a-scene').appendChild(newPiece);
       
       game.nextTurn();
