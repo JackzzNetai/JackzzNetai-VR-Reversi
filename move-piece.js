9a8d4bdd3679cd94005a9717c807e89d7447d618
Glitch (cse481-reversi-demo) <none>
2021-04-10 07:40:18 +0000

    🌁💺 Checkpoint ./move-piece.js:30458638/499

--- move-piece.js
+++ move-piece.js
@@ -16,15 +16,33 @@ AFRAME.registerComponent("move-piece", {
         return;
       }
 
+      //↑y
+      //  0.8
+      //  0.7
+      //  0.6
+      //  0.5
+      //  0.4
+      //  0.3
+      //  0.2
+      //  0.1
+      //  0.0   board center
+      
       let chosenPiece = intersectedEl.parentNode;
       let boardPosition = document.getElementById("board").getAttribute("position");
       gripped = true;
       setInterval(function() {
-        let d = boardPosition.y + 0.7;
+        let d = boardPosition.y + 0.4;
         let intersection = intersectionOfLaserAndBoard(el, d);
-        chosenPiece.object3D.position.x = intersection[0];
+        let x = intersection[0];
+        let z = intersection[2];
+        if (-4 < x && x < 4 && -4 < z && z < 4) { // within the board range
+          let neartestGrid = findNearestGridCenter(x, z);
+          x = neartestGrid[0];
+          z = neartestGrid[1];
+        }
+        chosenPiece.object3D.position.x = x;
         chosenPiece.object3D.position.y = boardPosition.y + 0.6;
-        chosenPiece.object3D.position.z = intersection[2];
+        chosenPiece.object3D.position.z = z;
       }, 2);
     };
 
